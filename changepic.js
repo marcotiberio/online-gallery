eec7b8ba28823147754789afe1bbc5ff37de8fff
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-23 16:23:35 +0100

    new page

--- changepic.js
+++ changepic.js
@@ -1,17 +1,18 @@
 // START
 function changePicStart() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('sticker-start').style.display = "none";
-  document.getElementById('infoModal').style.display = "none";
-  document.getElementById('contactModal').style.display = "none";
+  window.location.replace("index.html");
+  // document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
+  // document.getElementById('section').style.transition = "ease-in 0.2s";
+  // document.getElementById('sticker-start').style.display = "none";
+  // document.getElementById('infoModal').style.display = "none";
+  // document.getElementById('contactModal').style.display = "none";
 
-  document.getElementById('picname').style.display = "block";
-  document.getElementById('picname').style.transition = "ease-in 0.2s";
-  document.getElementById('picname').style.top = "44%";
-  document.getElementById('picname').style.left = "67%";
-  document.getElementById('picname-info').style.top = "44%";
-  document.getElementById('picname-info').style.left = "67%";
+  // document.getElementById('picname').style.display = "block";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.top = "44%";
+  // document.getElementById('picname').style.left = "67%";
+  // document.getElementById('picname-info').style.top = "44%";
+  // document.getElementById('picname-info').style.left = "67%";
 }
 
 // 001 - STRETCH
@@ -66,13 +67,7 @@ function changePic4() {
 
 // 005 - J
 function changePic5() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("page-j.html");
 }
 
 // 006 - HALO
