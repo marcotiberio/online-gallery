ff516b8f32514559ddfeb1ca21392a3d36aae60f
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-23 10:22:22 +0100

    change pic functions

--- changepic.js
+++ changepic.js
@@ -1,33 +1,262 @@
 // START
 function changePicStart() {
-    document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
-    document.getElementById('section').style.transition = "ease-in 0.2s";
-    document.getElementById('sticker-start').style.display = "none";
-    document.getElementById('infoModal').style.display = "none";
-    document.getElementById('contactModal').style.display = "none";
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('sticker-start').style.display = "none";
+  document.getElementById('infoModal').style.display = "none";
+  document.getElementById('contactModal').style.display = "none";
 
-    document.getElementById('picname').style.display = "block";
-    document.getElementById('picname').style.transition = "ease-in 0.2s";
-    document.getElementById('picname').style.top = "44%";
-    document.getElementById('picname').style.left = "67%";
-    document.getElementById('#picname-info').style.top = "44%";
-    document.getElementById('#picname-info').style.left = "67%";
-  }
+  // document.getElementById('picname').style.display = "block";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.top = "44%";
+  // document.getElementById('picname').style.left = "67%";
+  // document.getElementById('#picname-info').style.top = "44%";
+  // document.getElementById('#picname-info').style.left = "67%";
+}
 
-// 002 - TORSO
-function changePic0() {
+// 001 - STRETCH
+function changePic1() {
   document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_stretch.jpg)";
   document.getElementById('section').style.transition = "ease-in 0.2s";
   document.getElementById('thumbnailsModal').style.display = "none";
   document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
 
-  document.getElementById('picname').style.transition = "ease-in 0.2s";
-  document.getElementById('picname').style.top = "30%";
-  document.getElementById('picname').style.left = "30%";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.top = "30%";
+  // document.getElementById('picname').style.left = "30%";
 
-  document.getElementById('#picname-info').style.display = "none";
-  document.getElementById('#picname-info').style.top = "44%";
-  document.getElementById('#picname-info').style.left = "67%";
+  // document.getElementById('#picname-info').style.display = "none";
+  // document.getElementById('#picname-info').style.top = "44%";
+  // document.getElementById('#picname-info').style.left = "67%";
+}
 
+// 002 - TORSO
+function changePic2() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_torso.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
 
+// 003 - SECRETE 1
+function changePic3() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_secrete1.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 004 - CURVE
+function changePic4() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_curve.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 005 - J
+function changePic5() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 006 - HALO
+function changePic6() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 007 - ECLIPSE
+function changePic7() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_eclipse.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 008 - TWO HATS
+function changePic8() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_two-hats.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 009 - ISHA
+function changePic9() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 010 - RABIA 2
+function changePic10() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 011 - ARC
+function changePic11() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 012 - DISK
+function changePic12() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 013 - ABSENT
+function changePic13() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_absent.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 014 - REVEAL
+function changePic14() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_reveal.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 015 - TINO WHITE HAT
+function changePic15() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino-white-hat.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 016 - HIKAGE
+function changePic16() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_hikage.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 017 - BASE CIRCLE
+function changePic17() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_bare-circle.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 018 - ICE
+function changePic18() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_ice.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 019 - BLINDED
+function changePic19() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_blinded.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 020 - TINOTENDA 3
+function changePic20() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tinotenda3.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 021 - TINO CLOSE UP
+function changePic21() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino-close-up.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
+}
+
+// 022 - TINO 4
+function changePic22() {
+  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino4.jpg)";
+  document.getElementById('section').style.transition = "ease-in 0.2s";
+  document.getElementById('thumbnailsModal').style.display = "none";
+  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
+  // document.getElementById('picname').style.transition = "ease-in 0.2s";
+  // document.getElementById('picname').style.left = "45%";
+  // document.getElementById('picname-info').style.left = "36.8%";
 }
