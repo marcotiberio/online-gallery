86fafb64885c2f179e871f8820cf3f0f7409b06a
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-23 17:16:55 +0100

    new pages

--- changepic.js
+++ changepic.js
@@ -17,242 +17,115 @@ function changePicStart() {
 
 // 001 - STRETCH
 function changePic1() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_stretch.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-
-  document.getElementById('picname').style.display = "block";
-  document.getElementById('picname').style.transition = "ease-in 0.2s";
-  document.getElementById('picname').style.top = "40%";
-  document.getElementById('picname').style.left = "40%";
-
-  document.getElementById('picname-info').style.display = "none";
-  document.getElementById('picname-info').style.top = "40%";
-  document.getElementById('picname-info').style.left = "31.7%";
+  window.location.replace("bastiaan-woudt_stretch.html");
 }
 
 // 002 - TORSO
 function changePic2() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_torso.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_torso.html");
 }
 
 // 003 - SECRETE 1
 function changePic3() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_secrete1.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_secrete1.html");
 }
 
 // 004 - CURVE
 function changePic4() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_curve.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_curve.html");
 }
 
 // 005 - J
 function changePic5() {
-  window.location.replace("page-j.html");
+  window.location.replace("bastiaan-woudt_j.html");
 }
 
 // 006 - HALO
 function changePic6() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("#");
 }
 
 // 007 - ECLIPSE
 function changePic7() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_eclipse.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_eclipse.html");
 }
 
 // 008 - TWO HATS
 function changePic8() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_two-hats.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_two-hats.html");
 }
 
 // 009 - ISHA
 function changePic9() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("#");
 }
 
 // 010 - RABIA 2
 function changePic10() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("#");
 }
 
 // 011 - ARC
 function changePic11() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_j.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("#");
 }
 
 // 012 - DISK
 function changePic12() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_disk.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_disk.html");
 }
 
 // 013 - ABSENT
 function changePic13() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_absent.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_absent.html");
 }
 
 // 014 - REVEAL
 function changePic14() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_reveal.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_reveal.html");
 }
 
 // 015 - TINO WHITE HAT
 function changePic15() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino-white-hat.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_tino-white-hat.html");
 }
 
 // 016 - HIKAGE
 function changePic16() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_hikage.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_hikage.html");
 }
 
 // 017 - BASE CIRCLE
 function changePic17() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_bare-circle.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_bare-circle.html");
 }
 
 // 018 - ICE
 function changePic18() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_ice.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
- 
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_ice.html");
 }
 
 // 019 - BLINDED
 function changePic19() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_blinded.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_blinded.html");
 }
 
 // 020 - TINOTENDA 3
 function changePic20() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tinotenda3.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_tinotenda3.html");
 }
 
 // 021 - TINO CLOSE UP
 function changePic21() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino-close-up.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_tino-close-up.html");
 }
 
 // 022 - TINO 4
 function changePic22() {
-  document.getElementById('section').style.backgroundImage="url(assets/backgrounds/background_tino4.jpg)";
-  document.getElementById('section').style.transition = "ease-in 0.2s";
-  document.getElementById('thumbnailsModal').style.display = "none";
-  document.getElementById('thumbnailsModal').style.transition = "ease-out 0.5s";
-  
-  document.getElementById('picname').style.display = "none";
-  document.getElementById('picname-info').style.display = "none";
+  window.location.replace("bastiaan-woudt_tino4.html");
+}
+
+// 024 - BOOKS
+function changePicBooks() {
+  window.open("https://www.artibooks.com/bastiaan-woudt");
 }
